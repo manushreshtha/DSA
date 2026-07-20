69b8b1ff0f90e1d3b029e075506c7e09cccc3853
Manu Shreshtha Yadav <manushreshtha28@gmail.com>
2026-07-20 22:35:25 +0530

    Time: 0 ms (100%), Space: 7.9 MB (91.3%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        int k=1;
+        string s = to_string(num);
+        string res=s;
+        solve(s,k,res,0);
+        int ans=stoi(res);
+        return ans;
+    }
+    void solve(string &s, int k, string &res, int start){
+        if(k==0 || start==s.size()-1) return;
+        int maxi=*max_element(s.begin()+start+1,s.end());
+        for(int i=start+1; i<s.size(); i++){
+            if(s[start]<s[i] && s[i]==maxi){
+                swap(s[start],s[i]);
+                if(s.compare(res)>0) res=s;
+                solve(s,k-1,res,start+1);
+                swap(s[start],s[i]);
+            }
+        }
+        solve(s,k,res,start+1);
+    }
+};
